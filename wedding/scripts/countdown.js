9a4aa4e444669eb62a381d6f094edadece92c1d5
reccho <enichols619@gmail.com>
2024-11-04 22:55:58 +0100

    site

---
+++ wedding/scripts/countdown.js
@@ -0,0 +1,19 @@
+const timeEvent = new Date("Sep 4, 2025 13:00:00").getTime();
+
+var countdownInterval = setInterval(function() {
+
+    const timeCurrent = new Date().getTime();
+    const timeRemain = timeEvent - timeCurrent;
+
+    const days = Math.floor(timeRemain / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemain % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeRemain % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeRemain % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    if (timeRemain < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "It Happened!";
+    }
+}, 1000); // Counter text renews every 1000 ms
