39fde6991acf8204b0b4b58ca80bcd9aa806d9bf
reccho <enichols619@gmail.com>
2024-11-11 13:53:56 +0100

    all

---
+++ wedding/scripts/rsvp.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("#rsvp-form")
+const submitButton = document.querySelector("#rsvp-submit")
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxzWWWTkBxLs-vw99MFN-X7p-03c9oQ_pOVMh4R7-TZ3t20FLT9-gtOEG7V9IO7y9sMYA/exec'
+
+form.addEventListener('submit', e => {
+    submitButton.disabled = true
+    e.preventDefault()
+    let requestBody = new FormData(form)
+    fetch(scriptURL, { method: 'POST', body: requestBody})
+        .then(response => {
+        alert('Success!', response)
+        submitButton.disabled = false
+    })
+        .catch(error => {
+        alert('Error!', error.message)
+        submitButton.disabled = false
+    })
+})
