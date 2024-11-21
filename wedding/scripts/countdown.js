d7b01beb454ee8a0e4d3e373a1a8dcebeb81961b
reccho <enichols619@gmail.com>
2024-11-21 22:38:45 +0100

    timer

--- wedding/scripts/countdown.js
+++ wedding/scripts/countdown.js
@@ -10,8 +10,8 @@ var countdownInterval = setInterval(function() {
     const minutes = Math.floor((timeRemain % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeRemain % (1000 * 60)) / 1000);
 
-    //document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-    document.getElementById("countdown").innerHTML = days + " days & " + hours + " hours";
+    //document.getElementById("countdown").innerHTML = days + " days & " + hours + " hours";
+    document.getElementById("countdown").innerHTML = days + " days, " + hours + " hours, " + minutes + "minutes, " + seconds + " seconds";
 
     if (timeRemain < 0) {
         clearInterval(x);
