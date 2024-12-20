fd792e7a18a3d6a3046cbca295a1d03be28fdaf9
reccho <enichols619@gmail.com>
2024-12-20 12:05:06 +0000

    Automated minify of 796c56ad5ec2820f47635dd357c8929e2d1bf846

--- wedding/scripts/rsvp.js
+++ wedding/scripts/rsvp.js
@@ -1,18 +1,1 @@
-const form = document.querySelector("#rsvp-form")
-const submitButton = document.querySelector("#rsvp-submit")
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxzWWWTkBxLs-vw99MFN-X7p-03c9oQ_pOVMh4R7-TZ3t20FLT9-gtOEG7V9IO7y9sMYA/exec'
-
-form.addEventListener('submit', e => {
-    submitButton.disabled = true
-    e.preventDefault()
-    let requestBody = new FormData(form)
-    fetch(scriptURL, { method: 'POST', body: requestBody})
-        .then(response => {
-        alert('Thank you!', response)
-        submitButton.disabled = false
-    })
-        .catch(error => {
-        alert('Error!', error.message)
-        submitButton.disabled = false
-    })
-})
+const form=document.querySelector("#rsvp-form"),submitButton=document.querySelector("#rsvp-submit"),scriptURL="https://script.google.com/macros/s/AKfycbxzWWWTkBxLs-vw99MFN-X7p-03c9oQ_pOVMh4R7-TZ3t20FLT9-gtOEG7V9IO7y9sMYA/exec";form.addEventListener("submit",(e=>{submitButton.disabled=!0,e.preventDefault();let requestBody=new FormData(form);fetch(scriptURL,{method:"POST",body:requestBody}).then((response=>{alert("Thank you!",response),submitButton.disabled=!1})).catch((error=>{alert("Error!",error.message),submitButton.disabled=!1}))}));
