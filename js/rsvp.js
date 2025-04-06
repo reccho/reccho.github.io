73bf1f5acc34a38218ee85cf38283682398993c2
recho <enichols619@gmail.com>
2025-04-06 18:48:04 +0200

    minify

--- js/rsvp.js
+++ js/rsvp.js
@@ -1,6 +1,6 @@
 const form = document.querySelector("#rsvp-form")
 const submitButton = document.querySelector("#rsvp-submit")
-const macroURL = 'https://script.google.com/macros/s/AKfycbxzWWWTkBxLs-vw99MFN-X7p-03c9oQ_pOVMh4R7-TZ3t20FLT9-gtOEG7V9IO7y9sMYA/exec'
+const macroURL = 'https://script.google.com/macros/s/AKfycbxIjQ-tpbNqa76wzcCl446LaYqFWZ-UqApKdHn2J1uSe-m5XFGTQAUYTMwtNLP8nYPvOw/exec'
 
 form.addEventListener('submit', e => {
   submitButton.disabled = true
