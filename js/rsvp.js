3318685bd44e6ef48cfbc5ef667d3e6f37648178
recho <enichols619@gmail.com>
2025-04-06 19:09:51 +0200

    rsvp

--- js/rsvp.js
+++ js/rsvp.js
@@ -1,18 +1,21 @@
-const form = document.querySelector("#rsvp-form")
-const submitButton = document.querySelector("#rsvp-submit")
-const macroURL = 'https://script.google.com/macros/s/AKfycbxIjQ-tpbNqa76wzcCl446LaYqFWZ-UqApKdHn2J1uSe-m5XFGTQAUYTMwtNLP8nYPvOw/exec'
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.querySelector("#rsvp-form")
+  const submitButton = document.querySelector("#rsvp-submit")
+  const macroURL = form.dataset.macro;
+  console.log(macroURL);
 
-form.addEventListener('submit', e => {
-  submitButton.disabled = true
-  e.preventDefault()
-  let requestBody = new FormData(form)
-  fetch(macroURL, { method: 'POST', body: requestBody})
-    .then(response => {
-    alert('Thank you!', response)
-    submitButton.disabled = false
+  form.addEventListener('submit', e => {
+    submitButton.disabled = true
+    e.preventDefault()
+    let requestBody = new FormData(form)
+    fetch(macroURL, { method: 'POST', body: requestBody})
+      .then(response => {
+      alert('Thank you!', response)
+      submitButton.disabled = false
+    })
+      .catch(error => {
+      alert('Error...', error.message)
+      submitButton.disabled = false
+    })
   })
-    .catch(error => {
-    alert('Error...', error.message)
-    submitButton.disabled = false
-  })
-})
+});
