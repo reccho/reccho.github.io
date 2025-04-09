c0692642ccb7efb4178c0b2966be112b373e353b
recho <enichols619@gmail.com>
2025-04-09 17:16:42 +0200

    faq flex

--- js/rsvp.js
+++ js/rsvp.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
   const form = document.querySelector("#rsvp-form")
   const submitButton = document.querySelector("#rsvp-submit")
   const macroURL = form.dataset.macro;
-  console.log(macroURL);
 
   form.addEventListener('submit', e => {
     submitButton.disabled = true
